aa2f1293aa754d8b026fde4cfdb260ba0e5957bb
SteelBucket <depnra1@gmail.com>
2024-03-25 22:18:29 +0530

    Boilerplate added

---
+++ RestAPI/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.conf import settings
+
+
+# Create serializers here.
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+        model = settings.AUTH_USER_MODEL
+        fields = ('email', 'user_name', 'first_name')
+        extra_kwargs = {'password': {'write_only': True}}
---
+++ RestAPI/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+app_name = 'blog_api'
+
+urlpatterns = [
+]
